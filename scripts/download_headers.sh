a734be93fc7192063c6b15223ce2b31386313eb1
kojix2 <2xijok@gmail.com>
2025-04-04 12:33:40 +0900

    Add header files for Llama

---
+++ scripts/download_headers.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/include/llama.h
+wget https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/ggml/include/ggml.h
+wget https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/ggml/include/ggml-cpu.h
+wget https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/ggml/include/ggml-backend.h
