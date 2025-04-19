9848add635d3f0c41322d87c559a43f922555fe1
kojix2 <2xijok@gmail.com>
2025-04-19 15:59:23 +0900

    Remove backward compatibility aliases for Sampler classes

--- src/llama/sampler.cr
+++ src/llama/sampler.cr
@@ -21,25 +21,3 @@ require "./sampler/top_n_sigma"
 require "./sampler/xtc"
 require "./sampler/infill"
 require "./sampler/grammar_lazy_patterns"
-
-# Backward compatibility aliases
-module Llama
-  # Old class names are aliased to the new module structure
-  alias GreedySampler = Sampler::Greedy
-  alias TopKSampler = Sampler::TopK
-  alias TopPSampler = Sampler::TopP
-  alias TempSampler = Sampler::Temp
-  alias DistSampler = Sampler::Dist
-  alias SamplerChain = Sampler::Chain
-  alias MinPSampler = Sampler::MinP
-  alias TypicalSampler = Sampler::Typical
-  alias MirostatSampler = Sampler::Mirostat
-  alias MirostatV2Sampler = Sampler::MirostatV2
-  alias GrammarSampler = Sampler::Grammar
-  alias PenaltiesSampler = Sampler::Penalties
-  alias TempExtSampler = Sampler::TempExt
-  alias TopNSigmaSampler = Sampler::TopNSigma
-  alias XtcSampler = Sampler::Xtc
-  alias InfillSampler = Sampler::Infill
-  alias GrammarLazyPatternsSampler = Sampler::GrammarLazyPatterns
-end
